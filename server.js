26da191ad1f62bb083932eb7f6eba9cc30e9c493
Pablo Morales <pablofmorales@gmail.com>
2014-02-15 22:33:03 -0300

    Setted basic resources

--- server.js
+++ server.js
@@ -2,18 +2,22 @@ var express = require('express');
 var server = express();
 
 server.get('/', function(request, response){
-
-  response.send('hello world');
-  console.log('test');
-
+  response.send('Deployer ');
 });
 
 
-server.get('/test', function(request, response){
-
-  response.send("test2");
+server.get('/version/:project/new', function(request, response) {
+  debugger;
+  response.send("3.20");
 });
 
+server.get('/version/:project/', function(request, response) {
+  debugger;
+  response.send("3.19");
+});
+
+server.post('/deploy/:project/', function( request, response) {
+});
 
 server.listen(3000);
 
